dd87b730770000105b4dcdd16946ac211f938abe
DS <dsathyakumar@users.noreply.github.com>
2024-04-06 19:40:57 -0600

    Create example-1-sync-single-write.js

---
+++ src/example-1/example-1-sync-single-write.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const server = http.createServer();
+const port = 8080;
+
+server.on('request', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.statusCode = 200;
+    res.write(`
+    <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Marko v3 Server Side Rendering</title>
+            </head>
+            <body>
+                <h1>------Before everything--------</h1>
+                Hello Frank! You have 30 messages!
+                <ul>
+                    <li style="color: red"> Red </li>
+                    <li style="color: green"> Green </li>
+                    <li style="color: blue"> Blue </li>
+                </ul>
+                <h1>------After everything--------</h1>
+            </body>
+        </html>`);
+    res.end();
+  });
+  
+  server.listen(port, () => {
+    console.log(`Successfully started server on port ${port}`);
+  });
